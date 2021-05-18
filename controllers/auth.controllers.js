c34fd83c48c24dc0591c1ffe31e6010d34d78be2
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-18 15:13:02 +0200

    FEAT: add auth controllers

---
+++ controllers/auth.controllers.js
@@ -0,0 +1,71 @@
+const passport = require('passport');
+
+//auth/register
+const registerPost =  (req, res, next) => {
+    const {email, password, username} = req.body;
+    console.log('Registrando usuario...');
+    if (!email || !password || !username) {
+        const error = new Error('User, email and password are required');
+        return res.json(error.message);
+    }
+    passport.authenticate('registro', (error, user) => {
+        if (error) {
+            return res.json(error.message);
+        }
+        const userRegister = user;
+        userRegister.password = null;
+
+        return res.json(userRegister);
+    })(req, res, next);
+};
+
+//auth/login
+const loginPost = (req, res, next) => {
+    const {username, email, password} = req.body;
+    console.log('Logueando al usuario...', req.body);
+
+    if(!email|| !password || !username) {
+        const error = new Error('User, email and password are required');
+        return res.json(error.message);
+    }
+    passport.authenticate('acceso', (error, user) => {
+        if(error) {
+            return res.json(error.message);
+        }
+
+        req.login(user, (error) => {
+            if(error) {
+                return res.send(error.message);
+            }
+            return res.send(user);
+        });
+
+        /* return res.json(user); */
+    })(req, res, next);
+};
+
+//auth/logout
+const logoutPost = (req, res) => {
+    console.log('req.user', req.user);
+
+    if(req.user) {
+        req.logout();
+
+        req.session.destroy(() => {
+            res.clearCookie('connect.sid');
+            return res.json('Usuario Deslogueado');
+        });
+    }else {
+        return res.json('No user found');
+    }
+};
+
+
+module.exports = {
+    registerPost,
+    loginPost,
+    logoutPost,
+};
+
+
+
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,68 +1,12 @@
 const express = require('express');
-const passport = require('passport');
-const User = require('../model/User');
+const authController = require('../controllers/auth.controllers');
 const router = express.Router();
 
-//auth/register
-router.post ('/register', (req, res, next) => {
-    const {email, password, username} = req.body;
-    console.log('Registrando usuario...');
-    if (!email || !password || !username) {
-        const error = new Error('User, email and password are required');
-        return res.json(error.message);
-    }
-    passport.authenticate('registro', (error, user) => {
-        if (error) {
-            return res.json(error.message);
-        }
-        const userRegister = user;
-        userRegister.password = null;
+router.post('/register', authController.registerPost);
 
-        return res.json(userRegister);
-    })(req, res, next);
-});
+router.post('/login', authController.loginPost);
 
-//auth/login
-router.post('/login', (req, res, next) => {
-    const {username, email, password} = req.body;
-    console.log('Logueando al usuario...', req.body);
-
-    if(!email|| !password || !username) {
-        const error = new Error('User, email and password are required');
-        return res.json(error.message);
-    }
-    passport.authenticate('acceso', (error, user) => {
-        if(error) {
-            return res.json(error.message);
-        }
-
-        req.login(user, (error) => {
-            if(error) {
-                return res.send(error.message);
-            }
-            return res.send(user);
-        });
-
-        /* return res.json(user); */
-    })(req, res, next);
-});
-
-//auth/logout
-
-router.post('/logout', (req, res) => {
-    console.log('req.user', req.user);
-
-    if(req.user) {
-        req.logout();
-
-        req.session.destroy(() => {
-            res.clearCookie('connect.sid');
-            return res.json('Usuario Deslogueado');
-        });
-    }else {
-        return res.json('No user found');
-    }
-});
+router.post('/logout', authController.logoutPost);
 
 
 module.exports = router;
