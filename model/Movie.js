a8d538064f3b5fe7b3c6cd6f585d4fa755d7c68f
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-21 10:38:33 +0200

    FEAT: add model movie

---
+++ model/Movie.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const movieSchema = new Schema({
+    title:{type: String, required: true},
+    genres: [{type: String, required: true}],
+    overwiew: {type: String, required: true},
+    releaseDate: {type: Number, required: true},
+    credits: [{name: {type: String, required: true}, role: {type: String, required: true}}],
+    runtime: {type: Number, required: true},
+    images: [{Poster:{type: String, required: true}, back:{type: String, required: true}}],
+    providers: {type: String, required: true},
+    popularity: {type: Number, required: true},
+    trailer: {type: String, required: true},
+}, {
+    timestamps: true,
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
