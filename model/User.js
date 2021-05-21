3cc48e1aa202f76f3a9d5a0b5eb131674f873595
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-21 10:37:40 +0200

    FEAT: add id_provider

--- model/User.js
+++ model/User.js
@@ -4,10 +4,12 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    id_provider: [{type: String}]
 }, {
     timestamps: true
 });
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
