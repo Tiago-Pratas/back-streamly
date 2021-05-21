1a6e630f27e314ffd213d9a2d3b6b4b9d404d950
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-21 11:13:19 +0200

    FEAT: add model season

---
+++ model/Season.js
@@ -0,0 +1,14 @@
+const mongoose= require('mongoose');
+const Schema = mongoose.Schema;
+const seasonSchema = new Schema({
+    tvShow: {type: mongoose.Types.ObjectId, ref: 'tvShow'},
+    episodes: [{type: mongoose.Types.ObjectId, ref: 'episode'}],
+    name: {type: String, required: true},
+    season: {type: Number, required: true},
+}, {
+    timestamps: true,
+});
+
+const Season = mongoose.model('Season', seasonSchema);
+
+module.exports = Season;
