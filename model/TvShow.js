bf4f4f39fc03939e809f1b68864e0a0527704664
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-21 10:50:11 +0200

    FEAT: add model TvShow

---
+++ model/TvShow.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const tvShowSchema = new Schema({
+    name: {type: String, required: true},
+    seasons: {type: String, required: true},
+    episodes: {type: String, required: true},
+    started: {type: String, required: true},
+    ended: {type: String, required: true},
+    providers: [{name: {type: String}, logo: {type: String, required: true}}],
+    language: {type: String, required: true},
+    tagline: {type: String, required: true},
+    image: [{poster: {type: String, required: true}, back: {type: String, required: true}}],
+    popularity: {type: Number, required: true},
+    networks: [{name: {type: String, required: true}, logo: {type: String, required: true}, country: {type: String, required: true}}],
+}, {
+    timestamps: true,
+});
+
+const TvShow = mongoose.model('TvShow', tvShowSchema);
+
+module.exports = TvShow;
+
