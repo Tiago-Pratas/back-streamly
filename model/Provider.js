2fff01f50d445b0fd4ca5fd032fad6558069e84a
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-05-21 10:39:13 +0200

    FEAT: add model provider

---
+++ model/Provider.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const providerSchema = new Schema({
+    name: {type: String, required: true},
+    apId: {type: String, required: true},
+    logo: {type: String, required: true},
+}, {
+    timestamps: true,
+});
+
+const Provider = mongoose.model('Provider', providerSchema);
+
+module.exports = Provider;
+
