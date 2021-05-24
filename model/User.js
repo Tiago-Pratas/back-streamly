75917431ea6d0cc0890cac73c022965249da3e3a
Tiago-Pratas <prattiago@gmail.com>
2021-05-24 09:27:57 +0200

    FEAT: add more fields to some models

--- model/User.js
+++ model/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     email: {type: String, required: true, unique: true, minLength: 6},
     password: {type: String, required: true, minLength: 8},
     id_provider: [ { type: mongoose.Types.ObjectId, ref: 'provider' } ],
-    isActive: {type:Boolean, default: false},
+    isActive: {type:Boolean, default: false, required: true},
 }, {
     timestamps: true
 });
