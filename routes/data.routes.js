ba99e0fec482ffa6cdec411cf005cd97e84d9900
Tiago-Pratas <prattiago@gmail.com>
2021-05-28 12:48:07 +0200

    FIX: add delete route

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const dataController = require('../controllers/data.controllers');
-
-
+const User = require('../model/User');
 
 router.post('/providers', dataController.providerPost);
 
 router.post('/favorites', dataController.favoritesPost);
 
-router.post('/deletefavorites', async (req, res) => {
-    return res.status(200).json('that was it for fucks sake');
+router.post('/deletefavorites', async (req, res, next) => {
+    try {
+        const updateUser = await User.findOneAndUpdate(
+            { email: req.body.email },
+            { $pull: { id_medias: req.body.id } },
+            { new: true }
+        );
+
+        console.log(req.body.id, updateUser.id_medias.length);
+
+        return res.status(200).json(updateUser);
+
+    } catch (err) {
+        next(err);
+    }
 });
 
 
